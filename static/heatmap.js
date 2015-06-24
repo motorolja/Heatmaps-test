e3f12e61f38000ca973fed0ca91b66f42af3d7c7
motorolja <rasmusholm957@hotmail.com>
2015-06-24 09:40:53 -0400

    removed unused function

--- static/heatmap.js
+++ static/heatmap.js
@@ -10,47 +10,6 @@ window.onload = function()
     initialize();
 };
 
-// fetches all signal data
-function fetch_signal_data()
-{
-    var req = new XMLHttpRequest();
-    req.open('GET', '/fetch_signal_data',true);
-
-    req.onload = function ()
-    {
-        if (req.status === 200)
-        {
-            console.log('all done: ' + req.status);
-
-            var data = JSON.parse(req.responseText);
-
-            // checks if the upload was successful                                                                                                                                                                   
-            if(data.success && data.data != null)
-            {
-             
-                var data = data.data;
-                    
-                for (var i = 0; i < data.length; ++i)
-		{
-			SignalData.push(new google.maps.LatLng(data[i][0],data[i][1]));
-                }
-
-                toggleHeatmap();
-                toggleHeatmap();
-            }
-            else
-            {
-                // this should not happen                                                                                                                                                                                
-                console.log('Something went terribly wrong...');
-            }
-	}
-
-    };
-    
-    req.open('GET', '/fetch_signal_data',true);
-    req.send();
-
-}
 
 // Opens a websocket for the user
 function openSocket()
@@ -58,31 +17,11 @@ function openSocket()
     if ( "WebSocket" in window )
     {
         var ws = new WebSocket("ws://" + document.domain + ":80/persistant_connection");
-//	var heartbeat_msg = '--heartbeat--', heartbeat_interval = null
 	var missed_heartbeats = 0;
 	console.log('Created WebSocket');
 	ws.onopen = function()
 	{
 	    console.log('opening a websocket');
-/*	    
-	    if (heartbeat_interval === null) {
-        	missed_heartbeats = 0;
-        	heartbeat_interval = setInterval(function() {
-            		try {
-                		missed_heartbeats++;
-                		if (missed_heartbeats >= 10)
-                    			throw new Error("Too many missed heartbeats.");
-                		ws.send(heartbeat_msg);
-            		} catch(e) {
-                		clearInterval(heartbeat_interval);
-                		heartbeat_interval = null;
-                		console.warn("Closing connection. Reason: " + e.message);
-                		ws.close();
-            			}
-            	}, 5000);
-    	    }
-*/
-	    console.log('Done with onOpen()');
 	};
 
 	ws.onerror = function(error)
@@ -109,11 +48,6 @@ function openSocket()
                 ws.close();
                 console.log('server said close');
 	    }
-	   else if (received_data.message === "--heartbeat--") 
-	    {
-        	// reset the counter for missed heartbeats
-        	missed_heartbeats = 0;
-    	    }
             else if (received_data.message === 'Found data')
             {
                 console.log("Got new entries through websocket with data");
